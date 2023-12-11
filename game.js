7fa8b45550e8f68980adc73a1dda9587ac8e8529
Katharine <katnuotio@gmail.com>
2023-12-11 09:37:34 -0330

    changes to javascript

--- game.js
+++ game.js
@@ -8,5 +8,36 @@ const questions =[
         { text: "2", correct:false},
 
     ]
+  },
+  {
+  question: "Which is a primary color",
+  answers: [
+      { text: "red", correct:true},
+      { text: "purple", correct:false},
+      { text: "green", correct:false},
+      { text: "white", correct:false},
+  ]
   }
-]
+
+
+
+];
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton= document.getElementById("next-btn");
+
+let currentQuestionIndex= 0;
+let score = 0; 
+
+function startQuiz(){
+    currentQuestionIndex = 0
+    nextButton.innerHTML = "Next";
+    showquestion ();
+}
+
+function showQuestion () {
+    let currentQuestion =questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex +1;
+    questionElement.innerHTML =questionNo + "." +currentQuestion.
+    question;
+}
