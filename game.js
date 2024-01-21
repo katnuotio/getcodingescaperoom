ff89615420b4e2d274a4ca2e2195cd75ab5664cf
Katharine <katnuotio@gmail.com>
2024-01-21 11:01:54 -0330

    changes

--- game.js
+++ game.js
@@ -207,34 +207,96 @@ function showQuestion() {
     let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
-if (currentQuestion.isTrueFalse){
+    if (currentQuestion.isTrueFalse) {
+        
+        const trueButton = document.createElement("button");
+        trueButton.innerHTML = "True";
+        trueButton.classList.add("btn");
+        answerButton.appendChild(trueButton);
+        trueButton.dataset.correct = true;
 
-} else if (currentQuestion.scrambledLettersContainer){
+        const falseButton = document.createElement("button");
+        falseButton.innerHTML = "False";
+        falseButton.classList.add("btn");
+        answerButton.appendChild(falseButton);
+        falseButton.dataset.correct = false;
 
-} else {
+        trueButton.addEventListener('click', selectAnswer);
+        falseButton.addEventListener('click', selectAnswer);
+    } else if (currentQuestion.scrambledWord) {
+     
+        const scrambledLettersContainer = document.createElement("div");
+        scrambledLettersContainer.innerHTML = currentQuestion.scrambledWord;
+        scrambledLettersContainer.id = "scrambled-letters-container";
+        scrambledLettersContainer.classList.add("scrambled-container");
+        scrambledLettersContainer.classList.add("btn");
+        answerButton.appendChild(scrambledLettersContainer);
 
-    const cluesContainer =document.createElement("div");
-    cluesContainer.id = "clues-container";
-    cluesContainer.classList.add("clues-container");
-    answerButton.appendChild(cluesContainer);
+        const inputField = document.createElement("input");
+        inputField.type = "text";
+        inputField.id = "answer-input";
+        answerButton.appendChild(inputField);
 
-    const cluesButton = document.createElement("button");
-    cluesButton.innerHTML = "Show Clues";
-    cluesButton.classList.add("btn");
-    cluesContainer.appendChild(cluesButton);
+        const submitButton = document.createElement("button");
+        submitButton.innerHTML = "I know the secret word!";
+        submitButton.classList.add("btn");
+        answerButton.appendChild(submitButton);
 
-    const cluesList = document.createElement("ul");
-    cluesList.classList.add("clues-list");
-
-    cluesButton.addEventListener("click", () => {
-        currentQuestion.clues.forEach ((clue) => {
-            const clueItem = document.createElement("li");
-            clueItem.innerHTML = clue;
-            cluesList.appendChild(clueItem);   
+        submitButton.addEventListener('click', () => {
+            const userAnswer = inputField.value.trim().toLowerCase();
+            checkScrambledAnswer(userAnswer);
         });
-        cluesButton.disabled = true;
-    });
+
+        function checkScrambledAnswer(userAnswer) {
+            const currentQuestion = questions[currentQuestionIndex];
+            if (userAnswer === currentQuestion.correctAnswer.toLowerCase()) {
+                nextButton.style.display = "block";
+            } else {
+                alert("Try Again!");
+            }
+        }
+    } else {
+      
+        currentQuestion.answers.forEach(answer => {
+            const button = document.createElement("button");
+            button.innerHTML = answer.text;
+            button.classList.add("btn");
+            answerButton.appendChild(button);
+            if (answer.correct) {
+                button.dataset.correct = answer.correct;
+            }
+            button.addEventListener('click', selectAnswer);
+        });
+    }
+
+    
+    if (currentQuestion.clues) {
+        const cluesContainer = document.createElement("div");
+        cluesContainer.id = "clues-container";
+        cluesContainer.classList.add("clues-container");
+        answerButton.appendChild(cluesContainer);
+
+        const cluesButton = document.createElement("button");
+        cluesButton.innerHTML = "Show Clues";
+        cluesButton.classList.add("btn");
+        cluesContainer.appendChild(cluesButton);
+
+        const cluesList = document.createElement("ul");
+        cluesList.classList.add("clues-list");
+        cluesContainer.appendChild(cluesList);
+
+        cluesButton.addEventListener("click", () => {
+            currentQuestion.clues.forEach(clue => {
+                const clueItem = document.createElement("li");
+                clueItem.innerHTML = clue;
+                cluesList.appendChild(clueItem);
+            });
+            cluesButton.disabled = true;
+        });
+    }
 }
+
+
 
 
 
