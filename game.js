b6405a8ad37e8e28ad5023a6b2d0d615bb882a66
Katharine <katnuotio@gmail.com>
2024-02-29 21:53:05 -0330

    changes

--- game.js
+++ game.js
@@ -496,3 +496,50 @@ function showQuestion() {
     
 
 startQuiz();
+const categories = {
+    trivia_categories: [
+    {id: 9, name: "General Knowledge"},
+    {id: 10, name: "Entertainment: Books"},
+    {id: 11, name: "Entertainment: Film"},
+    {id: 12, name: "Entertainment: Music"},
+    {id: 13, name: "Entertainment: Musicals & Theatres"},
+    {id: 14, name: "Entertainment: Television"},
+    {id: 15, name: "Entertainment: Video Games"},
+    {id: 16, name: "Entertainment: Board Games"}, 
+    {id: 17, name: "Science & Nature"},
+    {id: 18, name: "Science: Computers"},
+    {id: 19, name: "Science: Mathematics"},
+    {id: 20, name: "Mythology"},
+    {id: 21, name:  "Sports"}, 
+    {id: 22, name: "Geography"}, 
+    {id: 23, name: "History"},
+    {id: 24, name: "Politics"},
+    {id: 25, name:  "Art"},
+    {id: 26, name:  "Celebrities"},
+    {id: 27, name: "Animals"}, 
+    {id: 28, name: "Vehicles"},
+    {id: 29, name: "Entertainment: Comics"},
+    {id: 30, name: "Science: Gadgets"},
+    {id: 31, name: "Entertainment: Japanese Anime & Manga"},
+    {id: 32, name: "Entertainment: Cartoon & Animations"}, 
+    ],
+
+};
+
+function populateSelect() {
+var ele = document.getElementById('sel');
+for (var i = 0; i < birds.length; i++) {
+    // POPULATE SELECT ELEMENT WITH JSON.
+    ele.innerHTML = ele.innerHTML +
+        '<option>' + birds[i]['Bird_Name'] + '</option>';
+}
+}
+
+function show() {
+// GET THE SELECTED VALUE FROM <select> ELEMENT AND SHOW IT.
+var msg = document.getElementById('msg');
+for (var i = 0; i < birds.length; i++)
+{
+    msg.innerHTML = birds[i].ID + " " + birds[i].Bird_Name;
+}
+}
